49cfe2c654beb8e8b0ad3108cbf0c8d28f838a61
Jorge Camarero Vera <jcamarerov@gmail.com>
2021-04-15 21:33:04 +0200

    Variable number of last characters

--- Trimming/tests/trimming_test.py
+++ Trimming/tests/trimming_test.py
@@ -8,12 +8,12 @@ def test_trimming():
     input_file = "./tests/resources/sequence_sample.txt"
     output_file = "./tests/resources/output.txt"
     expected_output_file = "./tests/resources/expected_output.txt"
-    trimming(input_file, output_file)
+    trimming(input_file, output_file, 60)
     assert filecmp.cmp(output_file, expected_output_file)
 
 def test_bad_output():
     input_file = "./tests/resources/sequence_sample.txt"
     output_file = "./tests/resources/output.txt"
     expected_output_file = "./tests/resources/bad_output.txt"
-    trimming(input_file, output_file)
+    trimming(input_file, output_file, 60)
     assert not filecmp.cmp(output_file, expected_output_file)
